ccbd5b266be3d2a90208be4b1fbb4b1ca2e12359
gonasooc <doupler@gmail.com>
2022-07-19 09:40:37 +0900

    빅오 표기법 - 빅오 표현식의 단순화하기

--- main.js
+++ main.js
@@ -6,11 +6,17 @@
 //   return total;
 // }
 
-function addUpTo(n) {
-  return n * (n + 1) / 2;
-}
+// function addUpTo(n) {
+//   return n * (n + 1) / 2;
+// }
 
-let t1 = performance.now();
-addUpTo(1000000000);
-let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+// let t1 = performance.now();
+// addUpTo(1000000000);
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+
+// 6. 연산 갯수 세기
+// 7. 시간 복잡도 시각화하기
+// 8. 빅오에 대한 공식 소개
+// 9. 빅오 표현식의 단순화하기
