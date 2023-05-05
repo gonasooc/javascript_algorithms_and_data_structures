c2aad304eab341e7e680207c69482d7456db225f
gonasooc <doupler@gmail.com>
2023-05-05 11:35:14 +0900

    last commit

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ charCount("hello");
 //   o: 1
 // }
 
-charCount("Your PIN number is 1234!")
+charCount("Your PIN number is 1234!");
 // {
 //   1: 1,
 //   2: 1,
@@ -55,16 +55,38 @@ charCount("Your PIN number is 1234!")
 //   y: 1
 // }
 
-function charCount(str) {
-  // do something
-  // return an object with keys that are lowercase alphanumeric characters in the string: values should be the counts for those character string
-}
+// function charCount(str) {
+//   var obj = {};
+//   for(var i = 0; i < str.length; i++) {
+//     var char = str[i].toLowerCase();
+//     if(/[a-z0-9]/.test(char)) {
+//       if(obj[char] > 0) {
+//         obj[char]++;
+//       } else {
+//         obj[char] = 1;
+//       };
+//     }
+//   }
+//   return console.log(obj);
+// }
 
 function charCount(str) {
-  // make object to return at end
-  // loop over string, for each character...
-    // if the char is number/letter AND is a key in object, add one to count
-    // if the char is number/letter AND not in object, add it to object and set value to 1
-    // if character is something else (space, period, etc.) don't do anything
-  // return object at end
+  var obj = {};
+  for(var char of str) {
+    if(isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return console.log(obj);
+}
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if(!(code > 47 && code < 58) && // numeric (0-9)
+     !(code > 64 && code < 91) && // upper alpha (A-Z)
+     !(code > 96 && code < 123)) { // lower alpha (a-z)
+    return false;
+  }
+  return true;
 }
